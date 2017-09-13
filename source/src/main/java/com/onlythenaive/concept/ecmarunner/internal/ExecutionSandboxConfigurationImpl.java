60b7d240ec8cdcf9407f4312649d513d9fd296ad
onlythenaive <ilya.gubarev@gmail.com>
2017-09-13 17:57:37 +0300

    Sandbox configuration implementation added

---
+++ source/src/main/java/com/onlythenaive/concept/ecmarunner/internal/ExecutionSandboxConfigurationImpl.java
@@ -0,0 +1,9 @@
+package com.onlythenaive.concept.ecmarunner.internal;
+
+import com.onlythenaive.concept.ecmarunner.ExecutionSandboxConfiguration;
+import com.onlythenaive.concept.ecmarunner.InternalImplementation;
+
+@InternalImplementation
+public final class ExecutionSandboxConfigurationImpl implements ExecutionSandboxConfiguration {
+
+}
