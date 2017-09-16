c67f9842ab8c7938c1c82483fb492f2cd0b15c24
onlythenaive <ilya.gubarev@gmail.com>
2017-09-16 04:17:00 +0300

    Execution sandbox implementation updated to have a simple way to execute a script

--- source/src/main/java/com/onlythenaive/concept/ecmarunner/internal/ExecutionSandboxImpl.java
+++ source/src/main/java/com/onlythenaive/concept/ecmarunner/internal/ExecutionSandboxImpl.java
@@ -2,6 +2,10 @@ package com.onlythenaive.concept.ecmarunner.internal;
 
 import java.util.Arrays;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import com.onlythenaive.concept.ecmarunner.ExecutionInvoice;
 import com.onlythenaive.concept.ecmarunner.ExecutionResult;
 import com.onlythenaive.concept.ecmarunner.ExecutionSandbox;
@@ -11,11 +15,23 @@ import com.onlythenaive.concept.ecmarunner.InternalImplementation;
 @InternalImplementation
 public final class ExecutionSandboxImpl implements ExecutionSandbox {
 
+    private final ScriptEngine engine;
+
+    public ExecutionSandboxImpl() {
+        this.engine = new ScriptEngineManager().getEngineByName("nashorn");
+    }
+
     @Override
     public ExecutionResult execute(final ExecutionInvoice invoice) {
         if (invoice == null) {
             throw new NullPointerException("Execution invoice cannot be null");
         }
+        try {
+            engine.eval(invoice.getScript());
+        } catch (ScriptException exception) {
+            throw new RuntimeException("Script exception occurred", exception);
+        }
+
         // TODO: implement invoice execution
         return new ExecutionResultImpl(
                 invoice,
