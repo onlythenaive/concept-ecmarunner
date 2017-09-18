2da02e42efe147226967490d59e3d7da47589211
onlythenaive <ilya.gubarev@gmail.com>
2017-09-18 06:27:20 +0300

    Test categories added

---
+++ source/src/test/java/com/onlythenaive/concept/ecmarunner/convention/IntegrationTest.java
@@ -0,0 +1,10 @@
+package com.onlythenaive.concept.ecmarunner.convention;
+
+/**
+ * Marker interface for integration tests.
+ *
+ * @author Ilia Gubarev
+ */
+public interface IntegrationTest {
+
+}
---
+++ source/src/test/java/com/onlythenaive/concept/ecmarunner/convention/Specification.java
@@ -0,0 +1,10 @@
+package com.onlythenaive.concept.ecmarunner.convention;
+
+/**
+ * Marker interface for unit tests (specifications).
+ *
+ * @author Ilia Gubarev
+ */
+public interface Specification {
+
+}
