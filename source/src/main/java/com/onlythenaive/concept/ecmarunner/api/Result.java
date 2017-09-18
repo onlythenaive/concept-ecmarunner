9a59e81d22642b70a01de7acafc8632865707523
onlythenaive <ilya.gubarev@gmail.com>
2017-09-18 03:25:06 +0300

    Result improved (type matching, converting)

--- source/src/main/java/com/onlythenaive/concept/ecmarunner/api/Result.java
+++ source/src/main/java/com/onlythenaive/concept/ecmarunner/api/Result.java
@@ -7,11 +7,12 @@ import com.onlythenaive.concept.ecmarunner.convention.ImmutableByContract;
 import com.onlythenaive.concept.ecmarunner.convention.PublishedApi;
 
 /**
- * Script(s) execution result.
+ * Script execution result.
  *
  * Contains results of an invoice execution. Designed to be immutable.
  *
  * @see Invoice
+ * @see LogRecord
  * @see TerminationType
  * @see ResultValueType
  *
@@ -27,6 +28,20 @@ public final class Result {
     private final Object value;
     private final ResultValueType valueType;
 
+    /**
+     * Creates a new execution invoice result.
+     *
+     * @param invoice initial execution invoice.
+     * @param logRecords log records produced during execution of the invoice.
+     * @param terminationType execution termination type.
+     * @param value execution result value object.
+     * @param valueType result value type.
+     *
+     * @see Invoice
+     * @see LogRecord
+     * @see TerminationType
+     * @see ResultValueType
+     */
     public Result(final Invoice invoice,
                   final List<LogRecord> logRecords,
                   final TerminationType terminationType,
@@ -48,8 +63,10 @@ public final class Result {
         if (valueType == null) {
             throw new NullPointerException("Result value type cannot be null");
         }
-        // TODO: check if value type corresponds to the value
         this.valueType = valueType;
+        if (valueTypeMismatch()) {
+            throw new IllegalArgumentException("Value type and actual result value mismatch");
+        }
     }
 
     /**
@@ -91,8 +108,43 @@ public final class Result {
      * @return execution result value or <code>null</code>.
      */
     public Object getValue() {
-        // TODO: analyze value type
         return this.value;
+    }
+
+    /**
+     * Gets a resulting value of this execution if any as <code>List</code> (copied).
+     *
+     * @return execution result value or <code>null</code>.
+     */
+    public List<?> getValueAsList() {
+        return new ArrayList<>((List<?>) this.value);
+    }
+
+    /**
+     * Gets a resulting value of this execution if any as <code>Boolean</code>.
+     *
+     * @return execution result value or <code>null</code>.
+     */
+    public Boolean getValueAsBoolean() {
+        return (Boolean) this.value;
+    }
+
+    /**
+     * Gets a resulting value of this execution if any as <code>Number</code>.
+     *
+     * @return execution result value or <code>null</code>.
+     */
+    public Number getValueAsNumber() {
+        return (Number) this.value;
+    }
+
+    /**
+     * Gets a resulting value of this execution if any as <code>String</code>.
+     *
+     * @return execution result value or <code>null</code>.
+     */
+    public String getValueAsString() {
+        return (String) this.value;
     }
 
     /**
@@ -104,5 +156,28 @@ public final class Result {
      */
     public ResultValueType getValueType() {
         return this.valueType;
+    }
+
+    private boolean valueTypeMatch() {
+        switch (valueType) {
+            case ARRAY:
+                return this.value instanceof List<?>;
+            case BOOLEAN:
+                return this.value instanceof Boolean;
+            case FUNCTION:
+            case NULL:
+            case UNDEFINED:
+                return this.value == null;
+            case NUMBER:
+                return this.value instanceof Number;
+            case STRING:
+                return this.value instanceof String;
+            default:
+                return true;
+        }
+    }
+
+    private boolean valueTypeMismatch() {
+        return !valueTypeMatch();
     }
 }
